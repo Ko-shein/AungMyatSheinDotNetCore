addb582e0cc293580b97549899c46ccb1a09ccfa
aungmyatshein <aungmyatshein@gmail.com>
2024-02-07 22:27:39 +0700

    Dapper CRUD

---
+++ DotNetTrainingBatch3.ConsoleApp/DapperExamples/DapperExample.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DotNetTrainingBatch3.ConsoleApp.Models;
+
+namespace DotNetTrainingBatch3.ConsoleApp.DapperExamples
+{
+    public class DapperExample
+    {
+        private readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "TestDb",
+            UserID = "sa",
+            Password = "shein@"
+        };
+        public void Read()
+        {
+            string query = @"SELECT [BlogId]
+                  ,[BlogTitle]
+                  ,[BlogAuthor]
+                  ,[BlogContent]
+              FROM [dbo].[Blog_Tb]";
+            using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            List<BlogModel> lst=db.Query<BlogModel>(query).ToList();
+            foreach (BlogModel item in lst)
+            {
+                Console.WriteLine("ID... " + item.BlogId);
+                Console.WriteLine("Title... " + item.BlogTitle);
+                Console.WriteLine("Author... " + item.BlogAuthor);
+                Console.WriteLine("Content... " + item.BlogContent);
+            }
+
+
+
+        }
+
+        public void Edit(int id)
+        {
+
+           
+            string query = @"SELECT [BlogId]
+                  ,[BlogTitle]
+                  ,[BlogAuthor]
+                  ,[BlogContent]
+              FROM [dbo].[Blog_Tb] Where BlogId = @BlogId";
+            using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            var item = db.Query<BlogModel>(query, new { BlogID = id }).FirstOrDefault();
+            if (item is null) { 
+               Console.WriteLine("Record not found");
+                return;
+            }
+            Console.WriteLine("ID... " + item.BlogId);
+            Console.WriteLine("Title... " + item.BlogTitle);
+            Console.WriteLine("Author... " + item.BlogAuthor);
+            Console.WriteLine("Content... " + item.BlogContent);
+         
+            
+            
+
+        }
+
+        public void Create(string title, string author, string content)
+
+        {
+           
+            string query = @"INSERT INTO [dbo].[Blog_Tb]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent])
+     VALUES
+           (@BlogTitle
+           ,@BlogAuthor
+           ,@BlogContent)";
+
+            BlogModel blogModel = new BlogModel()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+            using IDbConnection db = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, blogModel);
+            string message = result > 0 ? "Record inserted" : "Record not inserted";
+            Console.WriteLine(message);
+
+
+        }
+
+        public void Update(int id, string title, string author, string content)
+
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder.DataSource = ".";
+            sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+            sqlConnectionStringBuilder.UserID = "sa";
+            sqlConnectionStringBuilder.Password = "shein@";
+
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+
+
+            sqlConnection.Open();
+            string query = @"UPDATE [dbo].[Blog_Tb]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent 
+ WHERE BlogId = @BlogId";
+
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            cmd.Parameters.AddWithValue("@BlogTitle", title);
+            cmd.Parameters.AddWithValue("@BlogAuthor", author);
+            cmd.Parameters.AddWithValue("@BlogContent", content);
+            int result = cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            string message = result > 0 ? "Record updated" : "Record not updated";
+            Console.WriteLine(message);
+
+
+        }
+
+        public void Delete(int id)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder.DataSource = ".";
+            sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+            sqlConnectionStringBuilder.UserID = "sa";
+            sqlConnectionStringBuilder.Password = "shein@";
+
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+            sqlConnection.Open();
+            string query = @"Delete From [dbo].[Blog_Tb] WHERE BlogId = @BlogId";
+
+            SqlCommand cmd = new SqlCommand(query, sqlConnection);
+            cmd.Parameters.AddWithValue("@BlogId", id);
+            int result = cmd.ExecuteNonQuery();
+            sqlConnection.Close();
+
+            string message = result > 0 ? "Record deleted" : "Record not deleted";
+            Console.WriteLine(message);
+        }
+    }
+}
--- DotNetTrainingBatch3.ConsoleApp/Program.cs
+++ DotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -1,16 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DotNetTrainingBatch3.ConsoleApp.AdoDotNetExamples;
+using DotNetTrainingBatch3.ConsoleApp.DapperExamples;
 using System.Data;
 using System.Data.SqlClient;
 
 
 
+//AdoDotNetExample
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+////adoDotNetExample.Read();
+////adoDotNetExample.Edit(1);
+////adoDotNetExample.Edit(11);
+////adoDotNetExample.Create(title:"test title",author:"test author",content:"test content");
+////adoDotNetExample.Update(id: 7,title: "test title update", author: "test author update", content: "test content update");
+//adoDotNetExample.Delete(7);
+//Console.ReadKey();
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
-//adoDotNetExample.Read();
-//adoDotNetExample.Edit(1);
-//adoDotNetExample.Edit(11);
-//adoDotNetExample.Create(title:"test title",author:"test author",content:"test content");
-//adoDotNetExample.Update(id: 7,title: "test title update", author: "test author update", content: "test content update");
-adoDotNetExample.Delete(7);
+DapperExample dapperExample = new DapperExample();
+//dapperExample.Read();
+//dapperExample.Edit(1);
+//dapperExample.Edit(11);
+dapperExample.Create(title: "test title", author: "test author", content: "test content");
 Console.ReadKey();
