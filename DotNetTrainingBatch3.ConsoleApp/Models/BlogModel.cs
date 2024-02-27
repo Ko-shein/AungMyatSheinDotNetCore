273001ac7c8888ae8648da7df3a0262cd1df03fc
aungmyatshein <aungmyatshein@gmail.com>
2024-02-27 12:42:48 +0700

    WebApi CRUD

--- DotNetTrainingBatch3.ConsoleApp/Models/BlogModel.cs
+++ DotNetTrainingBatch3.ConsoleApp/Models/BlogModel.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DotNetTrainingBatch3.ConsoleApp.Models
 {
+    [Table ("Blog_Tb")]
     internal class BlogModel
     {
+        [Key]
         public int BlogId { get; set; }
         public string BlogTitle { get; set; }
         public string BlogAuthor { get; set; }
         public string BlogContent { get; set; }
+    }
+
+    public class JsonPlaceholderModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
     }
 }
--- DotNetTrainingBatch3.ConsoleApp/Program.cs
+++ DotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DotNetTrainingBatch3.ConsoleApp.AdoDotNetExamples;
 using DotNetTrainingBatch3.ConsoleApp.DapperExamples;
+using DotNetTrainingBatch3.ConsoleApp.EFCoreExamples;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -11,16 +12,24 @@ using System.Data.SqlClient;
 ////adoDotNetExample.Read();
 ////adoDotNetExample.Edit(1);
 ////adoDotNetExample.Edit(11);
-////adoDotNetExample.Create(title:"test title",author:"test author",content:"test content");
+////adoDotNetExample.Create(t itle:"test title",author:"test author",content:"test content");
 ////adoDotNetExample.Update(id: 7,title: "test title update", author: "test author update", content: "test content update");
 //adoDotNetExample.Delete(7);
 //Console.ReadKey();
 
-DapperExample dapperExample = new DapperExample();
+//DapperExample dapperExample = new DapperExample();
 //dapperExample.Read();
 //dapperExample.Edit(1);
 //dapperExample.Edit(11);
-dapperExample.Create(title: "test title", author: "test author", content: "test content");
-dapperExample.Update(id: 7,title: "test title update", author: "test author update", content: "test content update");
-dapperExample.Delete(7);
+//dapperExample.Create(title: "test title", author: "test author", content: "test content");
+//dapperExample.Update(id: 7, title: "test title update", author: "test author update", content: "test content update");
+//dapperExample.Delete(7);
+
+EFCoreExample eFCoreExample = new EFCoreExample();
+eFCoreExample.Read();
+eFCoreExample.Edit(1);
+eFCoreExample.Edit(11);
+eFCoreExample.Create(title: "test title", author: "test author", content: "test content");
+eFCoreExample.Update(id: 7, title: "test title update", author: "test author update", content: "test content update");
+eFCoreExample.Delete(7);
 Console.ReadKey();
