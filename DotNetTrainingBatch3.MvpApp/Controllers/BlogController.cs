9977d1be66aabd32c89c984ee3f129c9aee4b3a4
aungmyatshein <aungmyatshein@gmail.com>
2024-03-11 22:46:43 +0700

    MvcApp CRUD

---
+++ DotNetTrainingBatch3.MvpApp/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetTrainingBatch3.MvpApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult BlogIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ DotNetTrainingBatch3.MvpApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetTrainingBatch3.MvpApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
